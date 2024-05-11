190231f3708fd03b0a3c883376b4a8973862916c
Abinav <abinavananda@gmail.com>
2024-05-11 21:59:58 +0530

    first

---
+++ math/round(x, n).py
@@ -0,0 +1,3 @@
+import math
+print(round(4.56789, 2)) 
+print(round(4.56789))
---
+++ math/sin(x), cos(x), tan(x).py
@@ -0,0 +1,4 @@
+import math
+print(math.sin(math.radians(30)))  
+print(math.cos(math.radians(60)))  
+print(math.tan(math.radians(45)))
